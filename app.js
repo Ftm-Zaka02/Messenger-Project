edd010ef5883bc88c7687f2abe6682c4372aeac1
Fateme Zaka <ftmzakapr02@gmail.com>
2023-08-21 10:00:23 +0330

    add fonts & transition login

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const logintab=document.getElementById('login-tab')
-const signuptab=document.getElementById("signup-tab")
-signuptab.addEventListener("mouseover",function(e){
-    e.preventDefault();
-    logintab.classList.remove("tab-active")
-    signuptab.classList.add("tab-active")
-})
+const logintab = document.getElementById("login-tab");
+const signuptab = document.getElementById("signup-tab");
+signuptab.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  logintab.classList.remove("tab-active");
+  signuptab.classList.add("tab-active");
+  signuptab.style.transition = "all 1s";
+});
 
-logintab.addEventListener("mouseover",function(e){
-    e.preventDefault();
-    signuptab.classList.remove("tab-active")
-    logintab.classList.add("tab-active")
-})
-signuptab.addEventListener("mouseleave",function(e){
-    e.preventDefault();
-    signuptab.classList.remove("tab-active")
-    logintab.classList.add("tab-active")
-})
+logintab.addEventListener("mouseover", function (e) {
+  e.preventDefault();
+  signuptab.classList.remove("tab-active");
+  logintab.classList.add("tab-active");
+});
+signuptab.addEventListener("mouseleave", function (e) {
+  e.preventDefault();
+  signuptab.classList.remove("tab-active");
+  logintab.classList.add("tab-active");
+});

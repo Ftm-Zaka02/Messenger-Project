31afa2b77169978328dea905d3fa3927b22f5b85
Fateme Zaka <ftmzakapr02@gmail.com>
2023-09-08 22:15:44 +0330

    completing login & sign-up & creat chat page

--- app.js
+++ app.js
@@ -4,38 +4,24 @@ console.log(logintab.classList[0]);
 if (logintab.classList[1] == "tab-active") {
   signuptab.addEventListener("mouseover", function (e) {
     e.preventDefault();
-    logintab.classList.remove("tab-active");
-    signuptab.classList.add("tab-active");
+    signuptab.classList.add("tab-hover");
     signuptab.style.transition = "all 1s";
   });
 
-logintab.addEventListener("mouseover", function (e) {
+signuptab.addEventListener("mouseleave", function (e) {
   e.preventDefault();
-  signuptab.classList.remove("tab-active");
-  logintab.classList.add("tab-active");
+  signuptab.classList.remove("tab-hover");
   logintab.style.transition = "all 1s";
 });
-  signuptab.addEventListener("mouseleave", function (e) {
-    e.preventDefault();
-    signuptab.classList.remove("tab-active");
-    logintab.classList.add("tab-active");
-  });
 } else if (signuptab.classList[1] == "tab-active") {
   logintab.addEventListener("mouseover", function (e) {
     e.preventDefault();
-    signuptab.classList.remove("tab-active");
-    logintab.classList.add("tab-active");
+    logintab.classList.add("tab-hover");
     logintab.style.transition = "all 1s";
   });
 
-  signuptab.addEventListener("mouseover", function (e) {
-    e.preventDefault();
-    logintab.classList.remove("tab-active");
-    signuptab.classList.add("tab-active");
-  });
   logintab.addEventListener("mouseleave", function (e) {
     e.preventDefault();
-    logintab.classList.remove("tab-active");
-    signuptab.classList.add("tab-active");
+    logintab.classList.remove("tab-hover");
   });
 }

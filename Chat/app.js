52d78b510379d4b1f69e9fed955949402c5d7129
Fateme Zaka <ftmzakapr02@gmail.com>
2023-10-01 18:50:38 +0330

    edit & change pages

--- Chat/app.js
+++ Chat/app.js
@@ -2,6 +2,8 @@ const searchBtn = document.getElementById("search-btn");
 const searchDiv = document.getElementById("search-div");
 const searchInput = document.createElement("input");
 const searchCircle = document.createElement("button");
+
+//! search input & search function
 searchInput.placeholder = "Search";
 searchInput.classList.add("input-search");
 searchCircle.classList.add("search");
@@ -9,9 +11,31 @@ searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
   searchDiv.appendChild(searchInput);
   searchDiv.appendChild(searchCircle);
+  searchCircle.addEventListener("click", searchFunction);
 });
-const activeItem = document.getElementsByClassName("active-item")[0];
-const chatImg = document.createElement("img");
-chatImg.src = "images/chat.icon.png";
-chatImg.classList.add("chat-img");
-activeItem.appendChild(chatImg);
+function searchFunction() {
+  divs = document.querySelectorAll(".menu-item");
+  labels = document.querySelectorAll(".menu-item label");
+  const text = searchInput.value.toLowerCase();
+  for (let i = 0; i < labels.length; i++) {
+    const item = labels[i].textContent;
+    if (item.toLowerCase().indexOf(text) == -1) {
+      divs[i].style.display = "none";
+    }
+  }
+}
+
+//!send message
+messagePart = document.getElementsByClassName("message-part")[0];
+sendBtn = document.getElementById("send-btn");
+inputMessage = document.getElementsByName("input-message")[0];
+sendBtn.addEventListener("click", function () {
+  newMessage = document.createElement("div");
+  newMessage.classList.add("message-part-me");
+  textMessage = document.createTextNode(inputMessage.value);
+  if (textMessage != null && textContent != " ") {
+    newMessage.appendChild(textMessage);
+    messagePart.appendChild(newMessage);
+  }
+  inputMessage.value = "";
+});

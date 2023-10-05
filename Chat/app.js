7396801772976661d1996443b5114973586199c9
Fateme Zaka <ftmzakapr02@gmail.com>
2023-10-05 16:13:54 +0330

    add emoji & edit icons in chat page

--- Chat/app.js
+++ Chat/app.js
@@ -25,23 +25,56 @@ function searchFunction() {
 }
 
 //!send message
+let snjBtn = document.getElementById("sanjagh-btn");
+let mcpBtn = document.getElementById("micro-btn");
+let sendBtn = document.getElementById("send-btn");
 messagePart = document.getElementsByClassName("message-part")[0];
-sendBtn = document.getElementById("send-btn");
 inputMessage = document.getElementsByName("input-message")[0];
+
+inputMessage.addEventListener("keydown", function () {
+  snjBtn.style.display = "none";
+  mcpBtn.style.display = "none";
+  sendBtn.style.display = "block";
+});
+
 sendBtn.addEventListener("click", function () {
   newMessage = document.createElement("div");
   newMessage.classList.add("message-part-me");
   textMessage = document.createTextNode(inputMessage.value);
-  if (textMessage != null && textMessage != " ") {
+  if (textMessage.length>0) {
+    snjBtn.style.display = "block";
+    mcpBtn.style.display = "block";
+    sendBtn.style.display = "none";
     newMessage.appendChild(textMessage);
     messagePart.appendChild(newMessage);
   }
   inputMessage.value = "";
+  snjBtn.style.display = "block";
+  mcpBtn.style.display = "block";
+  sendBtn.style.display = "none";
 });
 
+let contants = document.getElementsByClassName("contants");
+contants[0].addEventListener("click", function () {
+  document
+    .getElementsByClassName("active-item")[0]
+    .classList.remove("active-item");
+  this.classList.add("active-item");
+});
 
-
-let addBtn=document.getElementById("add-btn")
-addBtn.addEventListener("click",function(){
-  
-})
+const emjBtn = document.getElementById("emoji-btn");
+const { createPicker } = window.picmo;
+inputMessage = document.getElementsByName("input-message")[0];
+const rootElement = document.querySelector("#picker");
+emjBtn.addEventListener("click", function () {
+  rootElement.style = "display:block";
+  const picker = createPicker({
+    rootElement,
+  });
+  picker.addEventListener("emoji:select", (selection) => {
+    inputMessage.value += selection.emoji;
+  });
+});
+inputMessage.addEventListener("mousedown", function () {
+  rootElement.style = "display:none";
+});

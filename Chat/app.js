93ed666fe0b1cc2a980ef978203d39cfa3ae9738
Fateme Zaka <ftmzakapr02@gmail.com>
2023-10-18 09:50:41 +0330

    add voice feature to  chat page

--- Chat/app.js
+++ Chat/app.js
@@ -1,3 +1,4 @@
+import WaveSurfer from "https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js";
 const searchBtn = document.getElementById("search-btn");
 const searchDiv = document.getElementById("search-div");
 const searchInput = document.createElement("input");
@@ -28,8 +29,8 @@ function searchFunction() {
 let snjBtn = document.getElementById("sanjagh-btn");
 let mcpBtn = document.getElementById("micro-btn");
 let sendBtn = document.getElementById("send-btn");
-messagePart = document.getElementsByClassName("message-part")[0];
-inputMessage = document.getElementsByName("input-message")[0];
+let messagePart = document.getElementsByClassName("message-part")[0];
+let inputMessage = document.getElementsByName("input-message")[0];
 
 inputMessage.addEventListener("keydown", function () {
   snjBtn.style.display = "none";
@@ -38,15 +39,12 @@ inputMessage.addEventListener("keydown", function () {
 });
 
 sendBtn.addEventListener("click", function () {
-  newMessage = document.createElement("div");
-  newMessage.classList.add("message-part-me");
-  textMessage = document.createTextNode(inputMessage.value);
-  if (textMessage.length>0) {
+  text = inputMessage.value;
+  if (text.length > 0) {
     snjBtn.style.display = "block";
     mcpBtn.style.display = "block";
     sendBtn.style.display = "none";
-    newMessage.appendChild(textMessage);
-    messagePart.appendChild(newMessage);
+    creatMessageDiv(text);
   }
   inputMessage.value = "";
   snjBtn.style.display = "block";
@@ -54,14 +52,57 @@ sendBtn.addEventListener("click", function () {
   sendBtn.style.display = "none";
 });
 
-let contants = document.getElementsByClassName("contants");
-contants[0].addEventListener("click", function () {
-  document
-    .getElementsByClassName("active-item")[0]
-    .classList.remove("active-item");
-  this.classList.add("active-item");
+function creatMessageDiv(text) {
+  textMessage = document.createTextNode(text);
+  newMessage = document.createElement("div");
+  newMessage.classList.add("message-part-me");
+  newMessage.appendChild(textMessage);
+  messagePart.appendChild(newMessage);
+}
+
+//
+
+//! change message with contants
+let contacts = document.getElementsByClassName("contacts")[0];
+contacts.addEventListener("click", function (e) {
+  let elm = e.target.id;
+  uploadMessage(elm);
 });
 
+function uploadMessage(num) {
+  for (let i = 0; i < contacts.children.length - 1; i++) {
+    if (num == i) {
+      document
+        .getElementsByClassName("active-item")[0]
+        .classList.remove("active-item");
+      contacts.children[i].classList.add("active-item");
+
+      fetch("../Jsons/myMessage.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let messageDivs = document.getElementsByClassName("message-part-me");
+          for (let j = 0; j < messageDivs.length; j++) {
+            messageDivs[j].innerHTML = data[i][j];
+          }
+        });
+
+      fetch("../Jsons/theirMessage.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let messageDivs = document.getElementsByClassName("message-part-you");
+          for (let j = 0; j < messageDivs.length; j++) {
+            messageDivs[j].innerHTML = data[i][j];
+          }
+        });
+    }
+  }
+}
+
+//!emoji
 const emjBtn = document.getElementById("emoji-btn");
 const { createPicker } = window.picmo;
 inputMessage = document.getElementsByName("input-message")[0];
@@ -77,4 +118,36 @@ emjBtn.addEventListener("click", function () {
 });
 inputMessage.addEventListener("mousedown", function () {
   rootElement.style = "display:none";
+});
+
+//!voice
+let microBtn = document.getElementById("micro-btn");
+let container = document.createElement("div");
+container.classList.add("voiceBox");
+microBtn.addEventListener("mousedown", function () {
+  const start = async () => {
+    let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    let recorder = new RecordRTCPromisesHandler(stream, {
+      type: "audio",
+    });
+    recorder.startRecording();
+    microBtn.addEventListener("mouseup", async () => {
+      await recorder.stopRecording();
+      let blob = await recorder.getBlob();
+      const recordedUrl = URL.createObjectURL(blob);
+      const wavesurfer = WaveSurfer.create({
+        container: container,
+        waveColor: "#fff",
+        progressColor: "#fff",
+        height: 60,
+        url: recordedUrl,
+      });
+
+      wavesurfer.on("interaction", () => {
+        wavesurfer.play();
+      });
+      messagePart.appendChild(container);
+    });
+  };
+  start();
 });
